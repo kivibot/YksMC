4598e5af5cecf2e0cb0449680e4a7829fe946297
Nicklas Ahlskog <kivi@kivibot.fi>
2017-05-26 02:35:42 +0300

    StreamMCPacketSink implementation

---
+++ YksMC.MCProtocol/StreamMCPacketSink.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using YksMC.MCProtocol.Utils;
+
+namespace YksMC.MCProtocol
+{
+    public class StreamMCPacketSink : IMCPacketSink, IDisposable
+    {
+        private readonly Stream _stream;
+
+        public StreamMCPacketSink(Stream stream)
+        {
+            _stream = stream;
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+
+        public async Task SendPacketAsync(byte[] data, CancellationToken cancelToken = default(CancellationToken))
+        {
+            byte[] packetLenghtData = VarIntUtil.EncodeVarInt(data.Length);
+            await _stream.WriteAsync(packetLenghtData, 0, packetLenghtData.Length, cancelToken);
+            await _stream.WriteAsync(data, 0, data.Length);
+        }
+    }
+}
---
+++ YksMc.MCProtocol.Tests/StreamMCPacketSinkTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YksMc.MCProtocol.Tests.Fakes;
+using YksMC.MCProtocol;
+using YksMC.MCProtocol.Utils;
+
+namespace YksMc.MCProtocol.Tests
+{
+    [TestFixture]
+    public class StreamMCPacketSinkTests
+    {
+
+        [Test]
+        public void TestDisposeDisposesTheUnderlyingStream()
+        {
+            FakeStream stream = new FakeStream();
+            StreamMCPacketSink sink = new StreamMCPacketSink(stream);
+
+            sink.Dispose();
+
+            Assert.IsTrue(stream.IsDisposed);
+        }
+
+        [Test]
+        public async Task TestSendPacketAsyncWithEmptyPacket()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamMCPacketSink sink = new StreamMCPacketSink(stream);
+
+            await sink.SendPacketAsync(new byte[0]);
+
+            Assert.AreEqual(VarIntUtil.EncodeVarInt(0), stream.ToArray());
+        }
+
+        [Test]
+        public async Task TestSendPacketAsyncSendsWholePacket()
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamMCPacketSink sink = new StreamMCPacketSink(stream);
+            List<byte> data = new List<byte>();
+            data.AddRange(new byte[500]);
+            data.Add(123);
+
+            await sink.SendPacketAsync(data.ToArray());
+
+            byte[] lengthData = VarIntUtil.EncodeVarInt(data.Count);
+            Assert.AreEqual(lengthData, stream.ToArray().Take(lengthData.Length));
+            Assert.AreEqual(data.Count + lengthData.Length, stream.ToArray().Length);
+            Assert.AreEqual(123, stream.ToArray().Last());
+        }
+
+    }
+}
