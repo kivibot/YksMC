926341b8e42fd60054e3eb41d2d446602c217739
Nicklas Ahlskog <kivi@kivibot.fi>
2017-10-11 13:42:12 +0300

    Chunk section performance improvements

--- YksMC.MineCraftModel/Chunk/ChunkSection.cs
+++ YksMC.MineCraftModel/Chunk/ChunkSection.cs
@@ -10,38 +10,49 @@ namespace YksMC.MinecraftModel.Chunk
         public const int Height = 16;
         public const int Width = 16;
 
-        private readonly IBlock[,,] _blocks;
+        private readonly IBlock[][][] _blocks;
 
         public ChunkSection(IBlock emptyBlock)
         {
-            IBlock[,,] blocks = new IBlock[Width, Height, Width];
-            for (int x = 0; x < Width; x++)
+            IBlock[] emptyRow = new IBlock[Width];
+            for (int i = 0; i < Width; i++)
             {
-                for (int y = 0; y < Height; y++)
-                {
-                    for (int z = 0; z < Width; z++)
-                    {
-                        blocks[x, y, z] = emptyBlock;
-                    }
-                }
+                emptyRow[i] = emptyBlock;
             }
+
+            IBlock[][] emptySlice = new IBlock[Height][];
+            for(int i=0; i<Height; i++)
+            {
+                emptySlice[i] = emptyRow;
+            }
+
+            IBlock[][][] blocks = new IBlock[Width][][];
+            for(int i=0; i<Width; i++)
+            {
+                blocks[i] = emptySlice;
+            }
+
             _blocks = blocks;
         }
 
-        public ChunkSection(IBlock[,,] blocks)
+        public ChunkSection(IBlock[][][] blocks)
         {
             _blocks = blocks;
         }
 
         public IBlock GetBlock(IBlockCoordinate position)
         {
-            return _blocks[position.X, position.Y % Height, position.Z];
+            return _blocks[position.X][position.Y % Height][position.Z];
         }
 
         public ChunkSection ChangeBlock(IBlockCoordinate position, IBlock block)
         {
-            IBlock[,,] blocks = (IBlock[,,])_blocks.Clone();
-            blocks[position.X % Chunk.Width, position.Y % Height, position.Z % Chunk.Width] = block;
+            IBlock[][][] blocks = (IBlock[][][])_blocks.Clone();
+            IBlock[][] slice = (IBlock[][])_blocks[position.X].Clone();
+            IBlock[] column = (IBlock[])_blocks[position.X][position.Y].Clone();
+            column[position.Z % Width] = block;
+            slice[position.Y % Height] = column;
+            blocks[position.X % Width] = slice;
             return new ChunkSection(blocks);
         }
     }
