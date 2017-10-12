824aafcd93433fe141a6e3133e772cd57da64577
Nicklas Ahlskog <kivi@kivibot.fi>
2017-10-12 19:33:15 +0300

    Multi block change handler implementation

--- YksMC.Bot/Handlers/BlockChangeHandler.cs
+++ YksMC.Bot/Handlers/BlockChangeHandler.cs
@@ -10,7 +10,7 @@ using YksMC.Protocol.Packets.Play.Clientbound;
 
 namespace YksMC.Bot.Handlers
 {
-    public class BlockChangeHandler : IWorldEventHandler<BlockChangePacket>
+    public class BlockChangeHandler : IWorldEventHandler<BlockChangePacket>, IWorldEventHandler<MultiBlockChangePacket>
     {
         private readonly IBlockTypeRepository _blockTypeRepository;
 
@@ -21,13 +21,6 @@ namespace YksMC.Bot.Handlers
 
         public IWorld ApplyEvent(BlockChangePacket packet, IWorld world)
         {
-            IBlockTypeIdentity blockTypeId = new BlockTypeIdentity(packet.BlockId >> 4, packet.BlockId & 0b1111);
-            IBlockType blockType = _blockTypeRepository.GetBlockType(blockTypeId);
-            if (blockType == null)
-            {
-                throw new ArgumentException($"Invalid block type: {blockTypeId}");
-            }
-
             IDimension dimension = world.GetCurrentDimension();
             if (dimension == null)
             {
@@ -37,9 +30,42 @@ namespace YksMC.Bot.Handlers
             IBlockCoordinate position = new BlockCoordinate(packet.Location.X, packet.Location.Y, packet.Location.Z);
             IChunkCoordinate chunkPosition = new ChunkCoordinate(position);
             IChunk chunk = dimension.GetChunk(chunkPosition);
-            IBlock block = chunk.GetBlock(position);
+            chunk = ReplaceBlockType(chunk, position, packet.BlockId);
 
-            return world.ReplaceCurrentDimension(dimension.ChangeChunk(chunkPosition, chunk.ChangeBlock(position, block.ChangeType(blockType))));
+            return world.ReplaceCurrentDimension(dimension.ChangeChunk(chunkPosition, chunk));
+        }
+
+        public IWorld ApplyEvent(MultiBlockChangePacket packet, IWorld world)
+        {
+            IDimension dimension = world.GetCurrentDimension();
+            if (dimension == null)
+            {
+                throw new ArgumentException("Dimension is loaded");
+            }
+
+            IChunkCoordinate chunkPosition = new ChunkCoordinate(packet.ChunkX, packet.ChunkZ);
+            IChunk chunk = dimension.GetChunk(chunkPosition);
+
+            foreach (MultiBlockChangePacketRecord record in packet.Records.Values)
+            {
+                IBlockCoordinate position = new BlockCoordinate(record.HorizontalPosition >> 4, record.Y, record.HorizontalPosition & 0xF);
+                chunk = ReplaceBlockType(chunk, position, record.BlockId);
+            }
+
+            return world.ReplaceCurrentDimension(dimension.ChangeChunk(chunkPosition, chunk));
+        }
+
+        private IChunk ReplaceBlockType(IChunk chunk, IBlockCoordinate position, int networkId)
+        {
+            IBlockTypeIdentity blockTypeId = new BlockTypeIdentity(networkId >> 4, networkId & 0b1111);
+            IBlockType blockType = _blockTypeRepository.GetBlockType(blockTypeId);
+            if (blockType == null)
+            {
+                throw new ArgumentException($"Invalid block type: {blockTypeId}");
+            }
+            IBlock block = chunk.GetBlock(position)
+                .ChangeType(blockType);
+            return chunk.ChangeBlock(position, block);
         }
     }
 }
---
+++ YksMC.Protocol.Packets/Play/Clientbound/MultiBlockChangePacket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YksMC.Protocol.Models.Attributes;
+using YksMC.Protocol.Models.Constants;
+using YksMC.Protocol.Models.Types;
+
+namespace YksMC.Protocol.Packets.Play.Clientbound
+{
+    [Packet(0x10, ConnectionState.Play, BoundTo.Client)]
+    public class MultiBlockChangePacket
+    {
+        public int ChunkX { get; set; }
+        public int ChunkZ { get; set; }
+        public VarArray<VarInt, MultiBlockChangePacketRecord> Records { get; set; }
+    }
+
+    public class MultiBlockChangePacketRecord
+    {
+        public byte HorizontalPosition { get; set; }
+        public byte Y { get; set; }
+        public VarInt BlockId { get; set; }
+    }
+}
