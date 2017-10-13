18533f136d54c84d8ed2752a9e96f50bcc36faf2
Nicklas Ahlskog <kivi@kivibot.fi>
2017-10-13 16:45:10 +0300

    Fixed the bot project tests

--- YksMC.Bot.Tests/Handlers/TimeUpdateHandlerTests.cs
+++ YksMC.Bot.Tests/Handlers/TimeUpdateHandlerTests.cs
@@ -29,10 +29,10 @@ namespace YksMC.Bot.Tests.Handlers
             };
             IWorld world = GetWorld();
 
-            handler.ApplyEvent(packet, world);
+            IWorld result = handler.ApplyEvent(packet, world);
 
-            Assert.AreEqual(12341337, world.GetCurrentDimension().AgeAndTime.AgeTicks);
-            Assert.AreEqual(1000, world.GetCurrentDimension().AgeAndTime.TimeTicks);
+            Assert.AreEqual(12341337, result.GetCurrentDimension().AgeAndTime.AgeTicks);
+            Assert.AreEqual(1000, result.GetCurrentDimension().AgeAndTime.TimeTicks);
         }
 
         private IWorld GetWorld()
@@ -42,7 +42,7 @@ namespace YksMC.Bot.Tests.Handlers
             IDimension dimension = new MinecraftModel.Dimension.Dimension(0, new DimensionType(true), emptyChunk);
             Dictionary<int, IDimension> dimensions = new Dictionary<int, IDimension>();
             dimensions[0] = dimension;
-            IWorld world = new World(new Dictionary<IPlayerId, IPlayer>(), null, dimensions, null);
+            IWorld world = new World(new Dictionary<IPlayerId, IPlayer>(), null, dimensions, dimension);
             return world;
         }
 
